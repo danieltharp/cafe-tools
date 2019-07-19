2095d9dea40dfa421cb79c4f63d94b62adf28848
bluesoul <github@nags.me>
2019-07-19 11:08:57 -0600

    Various improvements (header, flags, noupload arg)

--- episodereference.py
+++ episodereference.py
@@ -74,33 +74,33 @@ if "--newshow" in argv:
         print "Associating " + article + " to show."
         db.execute('INSERT INTO appearances VALUES(NULL,?,?,NULL)', (show_id, article_id))
         conn.commit()
-
-    for article in sluglist:
-        # Now, to stick in the Cafe archive.
-        # Correct workflow is create the page, then import the files.
-        page = s.pages.get_one({"site": config.wikidot_site, "page": article})
-        # Obey the speed limit.
-        sleep(0.25)
-        print "Saving page to wikidot."
-        page["tags"].append('_archived-article')
-        newpage = s.pages.save_one({"site": config.cafe_site, "page": page["fullname"], "title": page["title_shown"], "content": page["content"], "tags": page["tags"]})
-        sleep(0.25)
-        print "Getting list of files associated with " + article + "."
-        oldfiles = s.files.select({"site": config.wikidot_site, "page": page["fullname"]})
-        sleep(0.25)
-        for filename in oldfiles:
-            try:
-                print "Getting " + filename + "."
-            except UnicodeEncodeError:
-                pass
-            attachment = s.files.get_one({"site": config.wikidot_site, "page": page["fullname"], "file": filename})
+    if "--noupload" not in argv:
+        for article in sluglist:
+            # Now, to stick in the Cafe archive.
+            # Correct workflow is create the page, then import the files.
+            page = s.pages.get_one({"site": config.wikidot_site, "page": article})
+            # Obey the speed limit.
             sleep(0.25)
-            try:
-                print "Uploading " + filename + " to cafe wiki."
-            except UnicodeEncodeError:
-                pass
-            upload = s.files.save_one({"site": config.cafe_site, "page": page["fullname"], "file": filename,"content": attachment["content"], "comment": attachment["comment"], "revision_comment": "Uploaded with cafetools."})
+            print "Saving page to wikidot."
+            page["tags"].append('_archived-article')
+            newpage = s.pages.save_one({"site": config.cafe_site, "page": page["fullname"], "title": page["title_shown"], "content": page["content"], "tags": page["tags"]})
             sleep(0.25)
+            print "Getting list of files associated with " + article + "."
+            oldfiles = s.files.select({"site": config.wikidot_site, "page": page["fullname"]})
+            sleep(0.25)
+            for filename in oldfiles:
+                try:
+                    print "Getting " + filename + "."
+                except UnicodeEncodeError:
+                    pass
+                attachment = s.files.get_one({"site": config.wikidot_site, "page": page["fullname"], "file": filename})
+                sleep(0.25)
+                try:
+                    print "Uploading " + filename + " to cafe wiki."
+                except UnicodeEncodeError:
+                    pass
+                upload = s.files.save_one({"site": config.cafe_site, "page": page["fullname"], "file": filename,"content": attachment["content"], "comment": attachment["comment"], "revision_comment": "Uploaded with cafetools."})
+                sleep(0.25)
 
 def output():
     w = open("wikidot.txt","w+")
@@ -113,16 +113,30 @@ def output():
     h.close()
     h = open("html.txt", "a")
 
-    appearances = conn.execute("SELECT * FROM appearances JOIN shows on shows.id = appearances.show_id JOIN articles on articles.id = appearances.article_id ORDER BY id")
+    appearances = conn.execute("SELECT * FROM appearances JOIN shows on shows.id = appearances.show_id JOIN articles on articles.id = appearances.article_id ORDER BY shows.url")
     wikidotoutput = ""
     htmloutput = ""
 
-    w.write("||~ Episode ||~ Article ||~ Author ||\n")
+    w.write("//**NOTE:**// † indicates author's first positive-rated article. ¤ indicates bluesoul's personal favorites\n\n||~ Episode ||~ Article ||~ Author ||\n")
     h.write("<table class='wp-block-table tablesorter'><thead><tr><th>Episode ↕</th><th>Article ↕</th><th>Author ↕</th></tr></thead><tbody>")
 
     for row in appearances.fetchall():
-        w.write("||[*" + row[6] + " " + row[5] + "]||[*http://scp-wiki.net/" + row[9] + " " + row[10] + "] ([*http://scpcafe.wikidot.com/" + row[9] + " Archive])||[[*user " + row[11] + "]]||\n")
-        h.write("<tr><td><a href='" + row[6] + "' target='_blank' rel='noreferrer noopener' aria-label='" + row[5] + " (opens in a new tab)'>" + row[5] + "</a></td><td><a href='http://www.scp-wiki.net/" + row[9] + "' target='_blank' rel='noreferrer noopener' aria-label='" + row[10] + "(opens in a new tab)'>" + row[10] + "</a>          </td><td>" + row[11] + "</td></tr>")
+        w.write("||[*" + row[6] + " " + row[5] + "]||[*http://scp-wiki.net/" + row[9] + " " + row[10] + "] ([*http://scpcafe.wikidot.com/" + row[9] + " Archive])")
+        if row[13] is 1:
+            w.write(" †")
+        elif row[13] is 2:
+            w.write(" ¤")
+        elif row[13] is 3:
+            w.write(" †¤")
+        w.write("||[[*user " + row[11] + "]]||\n")
+        h.write("<tr><td><a href='" + row[6] + "' target='_blank' rel='noreferrer noopener' aria-label='" + row[5] + " (opens in a new tab)'>" + row[5] + "</a></td><td><a href='http://www.scp-wiki.net/" + row[9] + "' target='_blank' rel='noreferrer noopener' aria-label='" + row[10] + "(opens in a new tab)'>" + row[10] + "</a> (<a href='http://scpcafe.wikidot.com/" + row[9] + "Archive</a>)")
+        if row[13] is 1:
+            h.write(" †")
+        elif row[13] is 2:
+            h.write(" ¤")
+        elif row[13] is 3:
+            h.write(" †¤")
+        h.write("</td><td>" + row[11] + "</td></tr>")
 
     h.write("</tbody></table>")
 
